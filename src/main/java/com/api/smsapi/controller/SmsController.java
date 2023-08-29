d1702261b8980f5db55c9a4f548e089bbc9d0447
KANG\b2000 <hanqjun2660@gmail.com>
2023-08-29 17:23:34 +0900

    Controller -> RestController 변경

--- src/main/java/com/api/smsapi/controller/SmsController.java
+++ src/main/java/com/api/smsapi/controller/SmsController.java
@@ -5,17 +5,16 @@ import com.api.smsapi.dto.SmsResponseDTO;
 import com.api.smsapi.service.SmsService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
-@Controller
+@RestController
 public class SmsController {
 
     private SmsService smsService;
@@ -26,7 +25,6 @@ public class SmsController {
     }
 
     @PostMapping("/sms/send")
-    @ResponseBody
     public SmsResponseDTO sendSms(@RequestBody MessageDTO messageDTO) throws UnsupportedEncodingException, NoSuchAlgorithmException, URISyntaxException, JsonProcessingException, InvalidKeyException {
         SmsResponseDTO response = smsService.sendSms(messageDTO);
         return response;
